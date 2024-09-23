b23187af1a4b3d897e0f86a09450b1334b7626ec
aresCharcaC <henry.charca@tecsup.edu.pe>
2024-09-23 13:07:09 -0500

    codelab5

---
+++ app/src/main/java/com/example/codelab005/wellnessScreen.kt
@@ -0,0 +1,26 @@
+package com.example.codelab005
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WellnessScreen(
+    modifier: Modifier = Modifier,
+    wellnessViewModel: WellnessViewModel = viewModel()
+) {
+    Column(modifier = modifier) {
+        StatefulCounter()
+
+        WellnessTasksList(
+            list = wellnessViewModel.tasks,
+            onCheckedTask = { task, checked ->
+                wellnessViewModel.changeTaskChecked(task, checked)
+            },
+            onCloseTask = { task ->
+                wellnessViewModel.remove(task)
+            }
+        )
+    }
+}
+
